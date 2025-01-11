2c0785e509067a6fb5016afbc5a70dd53f886372
tot121212 <totmusicbusiness@gmail.com>
2025-01-10 19:43:29 -0600

    finished

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// this project is so bloated by the way i implemented player/symbols but its too late now (sunk cost fallacy)
+
 // FOR USER DEFINITION
 // Enum for symbols...
 const Symbols = {
@@ -5,9 +7,11 @@ const Symbols = {
     PLAYER1: 'X',
     PLAYER2: 'O'
 }
+
 const getAllSymbols = () => {
     return Object.values(Symbols).slice();
 }
+
 // for getting available symbols, excluding the empty one
 const getPlayerSymbols = () => {
     return getAllSymbols().slice(1);
@@ -15,7 +19,6 @@ const getPlayerSymbols = () => {
 
 const boardSize = 3;
 Object.defineProperty(Symbols, 'EMPTY', { writable: false, configurable: false }); // ensure empty is immutable
-// FOR USER DEFINITION
 
 const Player = (function(){
     const players = [];
@@ -26,9 +29,10 @@ const Player = (function(){
         currentPlayer = null;
     }
     
-    const newPlayer = (s) => {
+    const newPlayer = (symb, n) => {
         const index = players.length;
-        const symbol = String(s);
+        const symbol = String(symb);
+        const name = n;
     
         const getPlayerIndex = function(){
             return index;
@@ -36,8 +40,11 @@ const Player = (function(){
         const getSymbol = function(){
             return symbol;
         }
+        const getName = function(){
+            return name;
+        }
         
-        players.push({ getPlayerIndex, getSymbol });
+        players.push({ getPlayerIndex, getSymbol, getName });
         return players[index];
     }
 
@@ -162,70 +169,127 @@ const GameBoard = (function(){
     return { boardExists, initializeBoard, getBoardVisual, getBoardLength, setBoardSlotValue, getBoardSlotValue, getRows, getColumns, getDiagonals };
 })();
 
-const Leaderboard = (() => {
-
-})();
-
 const HtmlHandler = (function(){
-    const startListeningForPlayerInput = () => {
-        const tableElement = document.querySelector(".ttt-board");
-        tableElement.addEventListener('click', function(e){
-            playerInput = e.target.getAttribute('data-id');
-            if (typeof playerInput !== 'string') return;
-            if (!Game.getIsGameInProgress()) return; // check if game is running before player move
-            Game.onPlayerInput(playerInput);
-            if (Game.getIsGameInProgress()){ // check if player has won, if so we dont need to do any before operations bc game is over
-                Game.beforePlayerInput();
-            }
-        });
-    };
-
-    const initializeTable = (boardLength) => {
-        const board = document.querySelector('.ttt-board');
-        board.innerHTML = '';
-        for (let i = 0; i < boardLength; i++){
-            const row = document.createElement('tr');
-            for (let j = 0; j < boardLength; j++){
-                const cell = document.createElement('td');
-                cell.setAttribute('data-id', `${i}, ${j}`);
-                cell.innerHTML = Symbols.EMPTY;
-                row.appendChild(cell);
-            }
-            board.appendChild(row);
-        }
-    };
-
-    const getElementAt = (at) => {
-        [row, column] = at;
-        const cell = document.querySelector(`td[data-id="${row}, ${column}"]`);
-        if (!cell) throw new Error("HTML element does not exist");
-        return cell;
-    };
-
-    const updateTableElementAtWith = (at, w) => {
-        // query for specific element changes instead of updating the whole thing
-        [row, column] = at; // destructure row and column
-        const cell = document.querySelector(`td[data-id="${row}, ${column}"]`);
-        cell.textContent = w;
-        if (!cell.textContent === w) throw new Error("HTML element textContent was not updated");
-        return true;
-    };
-
-    // TODO: function to add victor class to td
-    const addVictorClassTo = (at) => {
-        cell = getElementAt(at);
-        cell.classList.add('victor');
-        return true;
+    const hideElement = (elementCssTags) => {
+        let element = document.querySelector(elementCssTags);
+        element.classList.add("hidden");
     }
 
-    const updateLeaderboard = () => {
-        // update leaderboard with each players victories, perhaps as a table
-        document.querySelector('#leaderboard').innerHTML = '';
-        // ideally in actual implementation it would be an sql database, csv file, or smthn but for now its just tied to the current instance of the html page
+    const showElement = (elementCssTags) => {
+        let element = document.querySelector(elementCssTags);
+        element.classList.remove("hidden");
+    }
+
+    const showHideHtmlInit = () => {
+        HtmlHandler.hideElement("#ttt-board");
+
+        HtmlHandler.showElement("#player-data-form");
+        HtmlHandler.showElement("button#start-game");
         
+        HtmlHandler.hideElement("#victory-info");
     }
 
-    return { initializeTable, updateTableElementAtWith, startListeningForPlayerInput, updateLeaderboard};
+    const showHideHtmlInGame = () => {
+        HtmlHandler.showElement("#ttt-board");
+
+        HtmlHandler.hideElement("#player-data-form");
+        HtmlHandler.hideElement("button#start-game");
+        
+        HtmlHandler.hideElement("#victory-info");
+    }
+
+    const showHideHtmlAfterGame = () => {
+        HtmlHandler.showElement("#ttt-board");
+
+        HtmlHandler.hideElement("#player-data-form");
+        HtmlHandler.showElement("button#start-game");
+
+        HtmlHandler.showElement("#victory-info");
+    }
+
+    const PlayerForm = (() => {
+        const initializePlayerInputForm = () => {
+            let playerDataForm = document.querySelector('#player-data-form');
+            playerDataForm.innerHTML = '';
+            let availableSymbols = getPlayerSymbols();
+            for (symbol of availableSymbols){
+                playerDataForm.innerHTML += `<input type="text" name="player-name" data-id="${symbol}" placeholder="Player Name for ${symbol}"></input>`;
+            }
+        }
+
+        const queryForPlayerName = (symbol) => {
+            let playerName = document.querySelector(`input[data-id="${symbol}"]`).value;
+            return playerName;
+        }
+
+        return { initializePlayerInputForm,queryForPlayerName };
+    })();
+
+    const GameBoard = (() => {
+        const startListeningForPlayerInput = () => { // dont need to ever stop bc we check if game is running
+            const tableElement = document.querySelector("#ttt-board");
+            tableElement.addEventListener('click', function(e){
+                if (!Game.getIsGameInProgress()) return; // check if game is running before anything
+                playerInput = e.target.getAttribute('data-id');
+                if (typeof playerInput !== 'string') return;
+                Game.onPlayerInput(playerInput);
+                if (Game.getIsGameInProgress()){ // check if player has won, if so we dont need to do any before operations bc game is over
+                    Game.beforePlayerInput();
+                }
+            });
+        };    
+        
+        const initializeTable = (boardLength) => {
+            const board = document.querySelector('#ttt-board');
+            board.innerHTML = '';
+            for (let i = 0; i < boardLength; i++){
+                const row = document.createElement('tr');
+                for (let j = 0; j < boardLength; j++){
+                    const cell = document.createElement('td');
+                    cell.setAttribute('data-id', `${i}, ${j}`);
+                    cell.innerHTML = Symbols.EMPTY;
+                    row.appendChild(cell);
+                }
+                board.appendChild(row);
+            }
+        };
+
+        const getElementAt = (at) => {
+            [row, column] = at;
+            const cell = document.querySelector(`td[data-id="${row}, ${column}"]`);
+            if (!cell) throw new Error("HTML element does not exist");
+            return cell;
+        };
+    
+        const updateTableElementAtWith = (at, w) => {
+            // query for specific element changes instead of updating the whole thing
+            [row, column] = at; // destructure row and column
+            const cell = document.querySelector(`td[data-id="${row}, ${column}"]`);
+            cell.textContent = w;
+            if (!cell.textContent === w) throw new Error("HTML element textContent was not updated");
+            return true;
+        };
+
+        return { startListeningForPlayerInput, initializeTable, getElementAt, updateTableElementAtWith };
+    })();
+
+    const VictoryInfo = (() => {
+        const updateVictoryInfo = (victoryInfo) => {
+            const element = document.querySelector('#victory-info');
+            const victorSymbol = victoryInfo.getVictorySymbol();
+            const player = victoryInfo.player;
+            if (victorSymbol === "No Victor"){
+                element.textContent = "No Victor";
+            }
+            else {
+                element.textContent = `${player.getName()} wins!`;
+            }
+        }
+
+        return { updateVictoryInfo };
+    })();
+    
+    return { hideElement, showElement, showHideHtmlAfterGame, showHideHtmlInit, showHideHtmlInGame, PlayerForm, GameBoard, VictoryInfo };
 })();
 
 const Game = (() => {
@@ -262,15 +326,16 @@ const Game = (() => {
     const checkForVictor = () => {
         let victorSymbol = null;
         const getVictorySymbol = () => { return victorSymbol };
+
         // put all rows, columns, and diagonals into an array for iteration
         const rows = GameBoard.getRows();
-
-        // check if victor
         const columns = GameBoard.getColumns();
         const diagonals = GameBoard.getDiagonals();
         const allPossibleVictories = rows.concat(columns).concat(diagonals);
+
         // gets enums for each player, excluding empty
         const playerSymbols = getPlayerSymbols();
+
         // for each row, column, and diagonal of board
         for (const segment of allPossibleVictories){
             if (playerSymbols.includes(segment[0]) && segment.slice(1).every(v => v === segment[0])){
@@ -278,14 +343,18 @@ const Game = (() => {
                 break;
             }
         }
+
+        if (victorSymbol){
+            const player = Player.getPlayer(playerSymbols.indexOf(victorSymbol));
+            return { getVictorySymbol, player };
+        }
+
         if (checkForNoVictor(rows)){
             victorSymbol = "No Victor";
+            return { getVictorySymbol, player: null };
         } 
         
-        if (victorSymbol){
-            
-            return { getVictorySymbol };
-        }
+        
         return null;
     }
 
@@ -331,12 +400,14 @@ const Game = (() => {
         console.log(`${Player.getCurrentPlayer().getSymbol()}'s turn`);
     }
 
-    const initializeGame = () => {
+    const startGame = () => {
         Player.resetPlayers();
         
         // intitialize new players
         for(symbol of getPlayerSymbols()){
-            let newPlayer = Player.newPlayer(symbol);
+            let name = HtmlHandler.PlayerForm.queryForPlayerName(symbol);
+            if (!name) {return false;}
+            let newPlayer = Player.newPlayer(symbol, name);
             console.log(newPlayer);
         }
 
@@ -347,22 +418,24 @@ const Game = (() => {
         Player.setCurrentPlayer(Player.getPlayers()[0]);
         console.log(`Current player: ${Player.getCurrentPlayer().getSymbol()}`);
 
-        HtmlHandler.initializeTable(GameBoard.getBoardLength());
+        HtmlHandler.GameBoard.initializeTable(GameBoard.getBoardLength());
 
         console.log("Game initialized");
         Game.setIsGameInProgress(true);
         Game.beforePlayerInput(); // for performing stuff before/between turns
+        return true;
     }
 
     const onPlayerVictory = (victoryInfo) => {
         const symbol = victoryInfo.getVictorySymbol();
-        console.log(victoryInfo);
         setIsGameInProgress(false);
         if (symbol === "No Victor"){
             console.log('No one won...');
         } else if (getPlayerSymbols().includes(symbol)){
             console.log(`${String(symbol)} won!`);
         }
+        HtmlHandler.VictoryInfo.updateVictoryInfo(victoryInfo);
+        HtmlHandler.showHideHtmlAfterGame();
     }
 
     const onPlayerInput = (playerInput) => {
@@ -387,7 +460,7 @@ const Game = (() => {
             console.log(error.message);
             return;
         }
-        HtmlHandler.updateTableElementAtWith(playerInput, boardSlotValue);
+        HtmlHandler.GameBoard.updateTableElementAtWith(playerInput, boardSlotValue);
 
         const victoryInfo = checkForVictor();
         if (victoryInfo){
@@ -397,43 +470,19 @@ const Game = (() => {
         Player.setCurrentPlayer(Player.getNextPlayer());
     }
 
-    /*
-    // function that is run once the game loop ends
-    
-    
-    // start game loop
-    const startGameLoop = () => {
-        while (true){
-            console.log(`${String(Player.getCurrentPlayer().getSymbol())}'s turn`);
-            
-            // ask for input
-            let unprocessedInput = HtmlHandler.createPlayerInputPromise();
-            console.log("Unprocessed input: " + String(unprocessedInput));
-            let playerInput = processPlayerInput(unprocessedInput);
-            if (!playerInput) { continue; } // make sure to continue if player input is invalid, allow them to try again
-            console.log("Player input: " + String(playerInput));
-
-            GameBoard.setBoardSlotValue(playerInput, Player.getCurrentPlayer().getSymbol()); // set board slot
-            (() => { // update html
-                const boardSlotValue = GameBoard.getBoardSlotValue(playerInput);
-                if (!boardSlotValue) { return null; }
-                HtmlHandler.updateTableElementAtWith(playerInput, boardSlotValue);
-            })();
-
-            const victoryInfo = checkForVictor();
-            if (victoryInfo && victoryInfo.victor ){ return victoryInfo; }
-            else {
-                Player.setCurrentPlayer((Player.getCurrentPlayer().getPlayerIndex() + 1) % Player.getPlayers().length);
-            }
-        }
-    }
-    */
-
-    return { getIsGameInProgress, setIsGameInProgress, initializeGame, beforePlayerInput, onPlayerInput, onPlayerVictory };
+    return { getIsGameInProgress, setIsGameInProgress, startGame, beforePlayerInput, onPlayerInput, onPlayerVictory };
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
-    Game.initializeGame();
-    HtmlHandler.startListeningForPlayerInput();
+    HtmlHandler.PlayerForm.initializePlayerInputForm();
+    HtmlHandler.showHideHtmlInit();
+    
+    HtmlHandler.GameBoard.startListeningForPlayerInput();
+    let startGameButton = document.querySelector("button#start-game");
+    startGameButton.addEventListener('click', () => {
+        Game.startGame();
+        HtmlHandler.showHideHtmlInGame();
+    });
 });
+
